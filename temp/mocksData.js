b9a27a5ce0c6b99812946f20dc0f9c1d9fae5f38
Goryachenok <goryachev.na72@gmail.com>
2022-10-11 12:33:08 +0500

    first release

---
+++ temp/mocksData.js
@@ -0,0 +1,132 @@
+'head': {
+    defaults: {
+
+    }
+}
+
+
+
+
+,
+
+__iconsData: {
+    
+        '19296707391540882614': {
+            width: '512px',
+            height: '512px'
+        },
+    
+        'arrow': {
+            width: '24px',
+            height: '13px'
+        },
+    
+        'arrowsvg': {
+            width: '24px',
+            height: '13px'
+        },
+    
+        'bac k': {
+            width: '512px',
+            height: '512px'
+        },
+    
+        'back': {
+            width: '512px',
+            height: '512px'
+        },
+    
+        'close-btn': {
+            width: '12px',
+            height: '12px'
+        },
+    
+        'close': {
+            width: '12px',
+            height: '12px'
+        },
+    
+        'download': {
+            width: '21px',
+            height: '31px'
+        },
+    
+        'logo-black': {
+            width: '138px',
+            height: '60px'
+        },
+    
+        'logo': {
+            width: '138px',
+            height: '60px'
+        },
+    
+        'mobile-arr': {
+            width: '35px',
+            height: '19px'
+        },
+    
+        'plus': {
+            width: '10px',
+            height: '10px'
+        },
+    
+        'telega': {
+            width: '14px',
+            height: '13px'
+        },
+    
+        'tg': {
+            width: '14px',
+            height: '13px'
+        },
+    
+        'up': {
+            width: '13px',
+            height: '15px'
+        },
+    
+        'whatsup': {
+            width: '15px',
+            height: '15px'
+        },
+    
+},
+
+__pages: [{
+                name: '!main',
+                href: '!main.html',
+                desc: 'Главная',
+             },{
+                name: '404',
+                href: '404.html',
+                desc: '404',
+             },{
+                name: 'about',
+                href: 'about.html',
+                desc: 'О проекте',
+             },{
+                name: 'become_the_owner',
+                href: 'become_the_owner.html',
+                desc: 'Стать владельцем',
+             },{
+                name: 'docs',
+                href: 'docs.html',
+                desc: 'Документы',
+             },{
+                name: 'index',
+                href: 'index.html',
+                desc: '//',
+             },{
+                name: 'infrastructure',
+                href: 'infrastructure.html',
+                desc: 'Инфраструктура',
+             },{
+                name: 'page',
+                href: 'page.html',
+                desc: 'Текстовая страница',
+             },{
+                name: 'plans',
+                href: 'plans.html',
+                desc: 'Планировки',
+             }]
---
+++ temp/symbols-data-template.js
@@ -0,0 +1,83 @@
+__iconsData: {
+    
+        '19296707391540882614': {
+            width: '512px',
+            height: '512px'
+        },
+    
+        'arrow': {
+            width: '24px',
+            height: '13px'
+        },
+    
+        'arrowsvg': {
+            width: '24px',
+            height: '13px'
+        },
+    
+        'bac k': {
+            width: '512px',
+            height: '512px'
+        },
+    
+        'back': {
+            width: '512px',
+            height: '512px'
+        },
+    
+        'close-btn': {
+            width: '12px',
+            height: '12px'
+        },
+    
+        'close': {
+            width: '12px',
+            height: '12px'
+        },
+    
+        'download': {
+            width: '21px',
+            height: '31px'
+        },
+    
+        'logo-black': {
+            width: '138px',
+            height: '60px'
+        },
+    
+        'logo': {
+            width: '138px',
+            height: '60px'
+        },
+    
+        'mobile-arr': {
+            width: '35px',
+            height: '19px'
+        },
+    
+        'plus': {
+            width: '10px',
+            height: '10px'
+        },
+    
+        'telega': {
+            width: '14px',
+            height: '13px'
+        },
+    
+        'tg': {
+            width: '14px',
+            height: '13px'
+        },
+    
+        'up': {
+            width: '13px',
+            height: '15px'
+        },
+    
+        'whatsup': {
+            width: '15px',
+            height: '15px'
+        },
+    
+}
